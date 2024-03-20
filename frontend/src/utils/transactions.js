4edc5add7497fb7b77332ebdc1aeb5198d38b89a
emargetis <erik.margetis@gmail.com>
2024-03-19 19:18:58 -0500

    feat (front-end SDK):
    Updated the subscribe methods to track multiple keys that are used in a "query" passed to the useSubscribe method
    
    Added a queryTransaction class which has methods `get`, `has` and `isEmpty` which can be used inside of a query. The queryTransaction class also keeps track of methods executed to create a list of multiple keys that a query depends on.
    
    Updated App.jsx to use a query instead of a single property name.
    
    Co-authored-by: Alex Sklar <sklarfox@gmail.com>
    Co-authored-by: Franco Waisfeld <francowaisfeld@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>

--- frontend/src/utils/transactions.js
+++ frontend/src/utils/transactions.js
@@ -3,29 +3,52 @@ import { monotonicFactory } from 'ulidx';
 const ulid = monotonicFactory();
 
 export class Transaction {
-  constructor(localState, notify, mutator, args, reason) {
+  constructor(localState, mutator, args, keysAccessed) {
     this.id = ulid(Date.now());
     this.localState = localState;
-    this.notify = notify;
     this.mutator = mutator;
     this.mutatorArgs = args;
-    this.reason = reason;
+    this.keysAccessed = keysAccessed;
   }
 
   get(key) {
+    this.keysAccessed[key] = true;
     return this.localState[key];
   }
 
   set(key, value) {
-    if (this.reason === 'initial') {
-      this.localState[key] = value; //update local KV
-      this.notify(key, { ...this.localState }); //alert subscribers of change
-    } else if (this.reason === 'replay') {
-      this.localState[key] = value; //update local KV
-    }
+    this.localState[key] = value; //update local KV
+    this.keysAccessed[key] = true;
   }
 
   delete(key) {
     delete this.localState[key];
+    this.keysAccessed[key] = true;
   }
+}
+
+export class QueryTransaction {
+  constructor(localState, keysAccessed) {
+    this.localState = localState;
+    this.keysAccessed = keysAccessed;
+  }
+  //get - returns the value for the key
+  get(key) {
+    this.keysAccessed[key] = true;
+    return this.localState[key];
+  }
+
+  //has - returns true if the key exists
+  has(key) {
+    this.keysAccessed[key] = true;
+    return Object.hasOwn(this.localState, key);
+  }
+
+  //isEmpty - returns true if the state is empty
+  isEmpty() {
+    return Object.keys(this.localState).length === 0;
+  }
+
+  //scan - to be implemented later, but returns an iterator, 
+  //useful for getting or mutating multiple keys based on some criteria
 }
