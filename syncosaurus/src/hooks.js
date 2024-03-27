784b8cfca4ddefd995be8f895cdebecb184a2f7a
Alex Sklar <sklarfox@gmail.com>
2024-03-27 15:16:18 -0700

    refactor(comments): remove wip comments
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>
    Co-authored-by: Franco Waisfeld <Francowaisfeld@gmail.com>

--- syncosaurus/src/hooks.js
+++ syncosaurus/src/hooks.js
@@ -1,29 +1,22 @@
 import { useState, useEffect } from 'react';
 
-//custom hook that returns data when the data the query relies on changes
 function useSubscribe(syncosaurus, query, initial) {
-  // State to store the key value pair
   const [data, setData] = useState(null);
 
-  //runs when the component is rendered initially or the syncosaurus / query objects change
   useEffect(() => {
-    // Subscribe to data using the provided syncosaurus subscription manager
     const unsubscribe = syncosaurus.subscribe(query, newData => {
       setData(newData);
     });
 
-    // Cleanup function to unsubscribe when the component unmounts
     return () => {
       unsubscribe();
     };
   }, []);
 
-  //if data is still null, then return the default value
   if (data === null) {
     return initial;
   }
 
-  //otherwise return the data
   return data;
 }
 
