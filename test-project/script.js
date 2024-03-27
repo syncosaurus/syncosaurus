4590538527a8d8147fb8e88264522510308bfd7b
Alex Sklar <sklarfox@gmail.com>
2024-03-27 15:14:13 -0700

    update version number
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>
    Co-authored-by: Franco Waisfeld <Francowaisfeld@gmail.com>

--- test-project/script.js
+++
@@ -1,5 +0,0 @@
-import Syncosaurus from 'syncosaurus';
-import { useSubscribe, usePresence, useUpdateMyPresence } from 'syncosaurus';
-
-console.log(Syncosaurus);
-console.log(useSubscribe, usePresence, useUpdateMyPresence);
