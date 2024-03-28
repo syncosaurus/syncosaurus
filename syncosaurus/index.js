800cae2a80c71a50841f7b73b2764349cb5859bc
Alex Sklar <sklarfox@gmail.com>
2024-03-27 18:38:16 -0700

    refactor(npm): move syncosaurus framework files into src
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>
    Co-authored-by: Franco Waisfeld <Francowaisfeld@gmail.com>

---
+++ syncosaurus/index.js
@@ -0,0 +1,6 @@
+import Syncosaurus from './syncosaurus.js';
+import { useSubscribe, usePresence, useUpdateMyPresence } from './hooks.js';
+
+export default Syncosaurus;
+
+export { Syncosaurus, useSubscribe, usePresence, useUpdateMyPresence };
